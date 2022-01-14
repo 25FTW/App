3932187b557d6a4f840b9d1c39ee46d42fb5e25f
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2022-01-14 15:56:13 +0530

    Added both the "Default" Page as well as the "Upload Image" Page.

--- lib/transaction_tabs/add_new_default.dart
+++ lib/transaction_tabs/add_new_default.dart
@@ -8,10 +8,75 @@ class AddNewDefault extends StatefulWidget {
 }
 
 class _AddNewDefault extends State<AddNewDefault> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Default"),
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.fromLTRB(10, 30, 0, 10),
+            child: Text(
+              "Item Name",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+            child: TextFormField(
+              cursorColor: Colors.green,
+              // The validator receives the text that the user has entered.
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(10, 15, 0, 10),
+            child: Text(
+              "Cost",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+            child: TextFormField(
+              cursorColor: Colors.green,
+              keyboardType:
+                  const TextInputType.numberWithOptions(decimal: true),
+              // The validator receives the text that the user has entered.
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')),
+                  );
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/transaction_tabs/add_new_image.dart
+++ lib/transaction_tabs/add_new_image.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddNewImage extends StatefulWidget {
   const AddNewImage({Key? key}) : super(key: key);
@@ -8,10 +10,72 @@ class AddNewImage extends StatefulWidget {
 }
 
 class _AddNewImage extends State<AddNewImage> {
+  File? _image;
+
+  Future getImageFromGallery() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (image == null) return;
+
+    final imageTemporary = File(image.path);
+    setState(() {
+      _image = imageTemporary;
+    });
+  }
+
+  Future getImageFromCamera() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.camera);
+    if (image == null) return;
+
+    final imageTemporary = File(image.path);
+    setState(() {
+      _image = imageTemporary;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Image"),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            _image != null
+                ? Image.file(
+                    _image!,
+                    height: 450,
+                  )
+                : const FlutterLogo(
+                    size: 100,
+                  ),
+            ElevatedButton.icon(
+              onPressed: getImageFromGallery,
+              icon: const Icon(
+                Icons.image,
+                size: 30,
+              ),
+              label: const Text("Upload Image from Gallery"),
+            ),
+            ElevatedButton.icon(
+              onPressed: getImageFromCamera,
+              icon: const Icon(
+                Icons.camera_alt_rounded,
+                size: 30,
+              ),
+              label: const Text("Take Image From Camera"),
+            ),
+            ElevatedButton.icon(
+              icon: const Icon(
+                Icons.upload_rounded,
+                size: 30,
+              ),
+              label: const Text("Upload"),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: getImage,
+      // ),
     );
   }
 }
