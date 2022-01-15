079470cf6ceff15ce440a03e683d23339bd9dd6a
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2022-01-15 15:12:57 +0530

    Added Boilerplate for the Dashboard Page.

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,4 +1,8 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'notifications.dart';
+import 'dart:convert';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -7,13 +11,220 @@ class DashboardPage extends StatefulWidget {
   State<DashboardPage> createState() => _DashboardPage();
 }
 
+String budgetURL = "http://10.0.2.2:5000/setBudget";
+String visualizationURL = "http://10.0.2.2:5000/analysis_monthly";
+
+class Data {
+  Map fetchedData = {
+    "data": [
+      {"value": 10, "title": "Reward"},
+    ]
+  };
+  late List _data;
+
+//function to fetch the data
+
+  Data() {
+    _data = fetchedData["data"];
+  }
+
+  String getTitle(int index) {
+    return _data[index]["title"];
+  }
+
+  num getValue(int index) {
+    return _data[index]["value"];
+  }
+
+  void addElement(String data, String value) {
+    _data.add({"name": data, "value": value});
+  }
+
+  int getLength() {
+    return _data.length;
+  }
+}
+
 class _DashboardPage extends State<DashboardPage> {
+  final Data _data = Data();
+  final _formKey = GlobalKey<FormState>();
+  num _newFunds = 1000;
+  final List<PieChartSectionData> _visualizationData = [];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Visualize Transactions"),
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Scaffold(
+          body: Column(
+            children: [
+              Form(
+                child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(15, 30, 0, 10),
+                      child: Text(
+                        "User: doodle1",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                      child: TextFormField(
+                        keyboardType: const TextInputType.numberWithOptions(
+                            decimal: true),
+                        cursorColor: Colors.green,
+                        // The validator receives the text that the user has entered.
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) => setState(() {
+                          _newFunds = value! as num;
+                        }),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16.0, vertical: 16.0),
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          // Validate returns true if the form is valid, or false otherwise.
+                          if (_formKey.currentState!.validate()) {
+                            // If the form is valid, display a snackbar. In the real world,
+                            // you'd often call a server or save the information in a database.
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Processing Data')),
+                            );
+                            _formKey.currentState?.save();
+                            // ignore: avoid_print
+                            print(_newFunds);
+
+                            var url = Uri.parse(budgetURL);
+                            var response = await http.post(url, body: {
+                              "username": "doodle1",
+                              "actual_item": _newFunds,
+                            });
+                            // ignore: avoid_print
+                            print('Response status: ${response.statusCode}');
+                            // ignore: avoid_print
+                            print('Response body: ${response.body}');
+                          }
+                        },
+                        child: const Text(
+                          'SUBMIT',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              PieChart(PieChartData(sections: _visualizationData)),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () async {
+              var url = Uri.parse(visualizationURL);
+              var response = await http.post(url,
+                  body: {"username": "doodle1", "month": 01, "year": 2022});
+              // ignore: avoid_print
+              print('Response status: ${response.statusCode}');
+              // ignore: avoid_print
+              print('Response body: ${response.body}');
+
+              Map<String, dynamic> parsedBody = json.decode(response.body);
+              num _totalFunds = 0;
+
+              parsedBody.forEach((key, value) {
+                // print("Key, Val: $key $value");
+                _totalFunds += value;
+                _data.addElement(value, key);
+                _visualizationData.add(
+                  PieChartSectionData(title: key, value: value),
+                );
+              });
+
+              setState(() {});
+
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const NotificationsPage()));
+            },
+            child: const Icon(Icons.add_alert_rounded),
+          ),
+        ),
       ),
     );
   }
 }
+
+//
+//   Widget _itemBuilder(BuildContext context, int index) {
+//     return InkWell(
+//       child: SizedBox(
+//         width: 500,
+//         child: Column(
+//           children: [
+//             (index == 0)
+//                 ? Column(
+//                     children: [
+//                       const Padding(
+//                         padding: EdgeInsets.fromLTRB(15, 30, 0, 10),
+//                         child: Text(
+//                           "User: doodle1",
+//                           style: TextStyle(
+//                               fontSize: 30, fontWeight: FontWeight.w600),
+//                         ),
+//                       ),
+//                       Padding(
+//                         padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+//                         child: TextFormField(
+//                           cursorColor: Colors.green,
+//                           // The validator receives the text that the user has entered.
+//                           validator: (value) {
+//                             if (value == null || value.isEmpty) {
+//                               return 'Please enter some text';
+//                             }
+//                             return null;
+//                           },
+//                           onSaved: (value) => setState(() {
+//                             _newFunds = value! as num;
+//                           }),
+//                         ),
+//                       ),
+//                     ],
+//                   )
+//                 : const Text(""),
+//             Card(
+//               shape: RoundedRectangleBorder(
+//                 borderRadius: BorderRadius.circular(15.0),
+//               ),
+//               color: Colors.pink,
+//               elevation: 10,
+//               child: Column(
+//                 mainAxisSize: MainAxisSize.max,
+//                 children: <Widget>[
+//                   ListTile(
+//                     leading: const Icon(Icons.account_balance_wallet_rounded,
+//                         size: 50),
+//                     title: Text("Name: ${_data.getName(index)}",
+//                         style:
+//                             const TextStyle(color: Colors.white, fontSize: 25)),
+//                     subtitle: Text("Value: ${_data.getValue(index)}",
+//                         style:
+//                             const TextStyle(color: Colors.white, fontSize: 18)),
+//                   )
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
