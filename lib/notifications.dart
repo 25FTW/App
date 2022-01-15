afa42d439606b865bed08aa9c94fa84dce354b81
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2022-01-15 11:57:12 +0530

    Notifications page completed.

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class NotificationsPage extends StatefulWidget {
   const NotificationsPage({Key? key}) : super(key: key);
@@ -8,15 +9,93 @@ class NotificationsPage extends StatefulWidget {
   State<NotificationsPage> createState() => _NotificationsPage();
 }
 
-const List<Tab> tabs = <Tab>[
-  Tab(text: 'Zeroth'),
-  Tab(text: 'First'),
-  Tab(text: 'Second'),
-];
+String mainUrl = "http://10.0.2.2:5000/getLogs";
+
+class Data {
+  Map fetchedData = {
+    "data": [
+      {"notification": "Joining Reward"},
+    ]
+  };
+  late List _data;
+
+//function to fetch the data
+
+  Data() {
+    _data = fetchedData["data"];
+  }
+
+  String getNotification(int index) {
+    return _data[index]["notification"];
+  }
+
+  void addElement(String data) {
+    _data.add({"notification": data});
+  }
+
+  int getLength() {
+    return _data.length;
+  }
+}
 
 class _NotificationsPage extends State<NotificationsPage> {
+  final Data _data = Data();
+
   @override
   Widget build(BuildContext context) {
-    return const Text("Hello");
+    return SafeArea(
+      child: Scaffold(
+        body: ListView.builder(
+          padding: const EdgeInsets.all(5.5),
+          itemCount: _data.getLength(),
+          itemBuilder: _itemBuilder,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            var url = Uri.parse(mainUrl);
+            var response = await http.post(url, body: {"username": "doodle1"});
+            // ignore: avoid_print
+            print('Response status: ${response.statusCode}');
+            // ignore: avoid_print
+            print('Response body: ${response.body}');
+
+            List parsedBody = json.decode(response.body.toString()) as List;
+            for (var key in parsedBody) {
+              // print("Key, Val: $key");
+              _data.addElement(key);
+            }
+
+            setState(() {});
+          },
+          child: const Icon(Icons.refresh_rounded),
+        ),
+      ),
+    );
+  }
+
+  Widget _itemBuilder(BuildContext context, int index) {
+    return InkWell(
+      child: SizedBox(
+        width: 500,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          color: Colors.pink,
+          elevation: 10,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              ListTile(
+                leading:
+                    const Icon(Icons.account_balance_wallet_rounded, size: 50),
+                title: Text("Name: ${_data.getNotification(index)}",
+                    style: const TextStyle(color: Colors.white, fontSize: 25)),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
